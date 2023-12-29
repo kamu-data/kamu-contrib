7ee19bb86e367d1026505d65e309ab94ce3c6d6e
Dima Pristupa <dima@pristupa.dev>
2023-12-29 18:59:40 +0200

    net.rocketpool: introduce 0.2.0 version (#13)
    * net.rocketpool: update app to respect ODF_BATCH_SIZE envvar
    * Support for wss & rpc endpoints
    * Request only selected events not all blocks
    * Use ethers-v5 instead of web3
    * Use typechain for contract typed code generation

--- net.rocketpool/image/src/app.ts
+++ net.rocketpool/image/src/app.ts
@@ -1,163 +1,239 @@
 import fs from "fs";
-import Web3 from "web3";
-import abiDecoder from "abi-decoder";
 import { Console } from "console";
+
+import { ethers } from "ethers"
+import { cleanEnv, url, str, num } from "envalid";
+
+import { RethAbi__factory } from "./contracts/factories/RethAbi__factory";
+import type { RethAbi } from "./contracts";
 
 const console = new Console(process.stderr);
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
-interface Event {
+interface EventRow {
   eventName: "TokensMinted" | "TokensBurned";
   amount: string;
   ethAmount: string;
   blockNumber: number;
   blockHash: string;
-  blockTime: number;
+  eventTime: number;
   transactionIndex: number;
   transactionHash: string;
   logIndex: number;
 }
 
+interface OdfReadRowsResult {
+  etag: number;
+  hasMore: boolean;
+}
+
+type Provider = ethers.providers.JsonRpcProvider | ethers.providers.WebSocketProvider;
+type OnEventsBatchCallback = (eventRows: ReadonlyArray<EventRow>) => boolean;
+type AllBlockRangeHasProcessed = boolean;
+
 /////////////////////////////////////////////////////////////////////////////////////////
 
-async function readEventsFromETH(
-  startBlock,
-  endBlock,
-  clb: (_: Event) => boolean,
-  stride = 10000
-) {
-  console.log(
-    "Considering events in block range [%s, %s] (%s blocks)",
-    startBlock,
-    endBlock,
-    endBlock - startBlock
-  );
+async function main() {
+  const env = cleanEnv(sanitizeEnvMiddleware(process.env), {
+    // Application:
+    ETH_NODE_PROVIDER_URL: url(),
+    BLOCK_BATCH_SIZE: num(),
+    // ODF interface:
+    ODF_BATCH_SIZE: num(),
+    ODF_ETAG: num({ default: 0 }),
+    ODF_NEW_ETAG_PATH: str(),
+    ODF_NEW_HAS_MORE_DATA_PATH: str(),
+  });
 
-  let fromBlock = startBlock;
-  let toBlock = fromBlock;
+  // See: https://etherscan.io/token/0xae78736cd615f374d3085123a210448e74fc6393
+  //      https://etherscan.io/address/0xae78736cd615f374d3085123a210448e74fc6393
+  const rethAddress = "0xae78736cd615f374d3085123a210448e74fc6393";
+  const rethStartBlock = 13325304;
 
-  do {
-    fromBlock = toBlock + 1;
-    toBlock = Math.min(endBlock, fromBlock + stride);
+  // Init
+  const providerUrl = env.ETH_NODE_PROVIDER_URL;
 
-    console.log("Syncing block range [%s, %s]", fromBlock, toBlock);
-    await readEventsFromETHRangeWithFallback(fromBlock, toBlock, clb);
-  } while (toBlock != endBlock);
+  console.log(`ETH node provider URL: ${providerUrl}`);
+
+  const provider = getProvider(providerUrl)
+  const rethContract = RethAbi__factory.connect(rethAddress, provider)
+
+  // Block range
+  const lastSeenBlock = env.ODF_ETAG;
+
+  console.log(`Last seen block: ${lastSeenBlock}`);
+
+  const ethHeadBlock = await provider.getBlockNumber();
+
+  console.log(`Current chain head block: ${ethHeadBlock}`);
+
+  const startBlock = Math.max(lastSeenBlock + 1, rethStartBlock);
+  const endBlock = ethHeadBlock;
+  const {
+    etag,
+    hasMore,
+  } = await odfReadRows(rethContract, startBlock, endBlock, env.BLOCK_BATCH_SIZE, env.ODF_BATCH_SIZE);
+
+  // Notify `kamu-cli` regarding new state
+  fs.writeFileSync(env.ODF_NEW_ETAG_PATH, etag.toString());
+
+  if (hasMore) {
+    fs.writeFileSync(env.ODF_NEW_HAS_MORE_DATA_PATH, "");
+  }
+
+  await destroy(provider);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
-async function readEventsFromETHRangeWithFallback(
-  fromBlock,
-  toBlock,
-  clb: (_: Event) => boolean
-) {
-  try {
-    const logs = await web3.eth.getPastLogs({
-      fromBlock,
-      toBlock,
-      address: rethAddress,
+async function odfReadRows(
+  rethContract: RethAbi,
+  startBlock: number,
+  endBlock: number,
+  blockBatchSize: number,
+  odfBatchSize: number,
+): Promise<OdfReadRowsResult> {
+  let outputRowsCount = 0;
+  let lastOutputBlockNumber = 0;
+
+  const allBlockRangeHasProcessed = await readRethContractEvents(
+    rethContract,
+    startBlock,
+    endBlock,
+    function (eventRows) {
+      for (const eventRow of eventRows) {
+        process.stdout.write(`${JSON.stringify(eventRow)}\n`);
+      }
+
+      outputRowsCount += eventRows.length;
+      lastOutputBlockNumber = eventRows[eventRows.length - 1].blockNumber;
+
+      const requestNextEventsBatch = outputRowsCount < odfBatchSize;
+
+      return requestNextEventsBatch;
+    },
+    blockBatchSize,
+  );
+
+  const etag = allBlockRangeHasProcessed ? endBlock : lastOutputBlockNumber;
+  const hasMore = !allBlockRangeHasProcessed;
+
+  return {
+    etag,
+    hasMore
+  };
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+async function readRethContractEvents(
+  rethContract: RethAbi,
+  startBlock: number,
+  endBlock: number,
+  onEventsBatchCallback: OnEventsBatchCallback,
+  blockBatchSize: number
+): Promise<AllBlockRangeHasProcessed> {
+  const blocksDelta = endBlock - startBlock;
+
+  console.log(`Considering events in block range [${startBlock}, ${endBlock}] (${blocksDelta} blocks)`);
+
+  let fromBlock = startBlock - 1;
+  let toBlock = fromBlock;
+
+  do {
+    fromBlock = toBlock + 1;
+    toBlock = Math.min(endBlock, fromBlock + blockBatchSize);
+
+    console.log(`Syncing block range [${fromBlock}, ${toBlock}]`);
+
+    const stopProcessing = !await readRethContractEventsBatch(
+      rethContract, fromBlock, toBlock, onEventsBatchCallback
+    );
+
+    if (stopProcessing) {
+      return false;
+    }
+  } while (toBlock !== endBlock);
+
+  return true;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+async function readRethContractEventsBatch(
+  rethContract: RethAbi,
+  fromBlock: number,
+  toBlock: number,
+  onEventsBatchCallback: OnEventsBatchCallback
+): Promise<AllBlockRangeHasProcessed> {
+  const eventFilters = [
+    rethContract.filters.TokensMinted(),
+    rethContract.filters.TokensBurned()
+  ] as const;
+  const eventsRowsPromises = eventFilters.map(async (eventFilter) => {
+    const events = await rethContract.queryFilter(eventFilter, fromBlock, toBlock);
+
+    return events.map((event) => {
+      return {
+        eventName: event.event,
+        amount: event.args.amount.toString(),
+        ethAmount: event.args.ethAmount.toString(),
+        blockNumber: event.blockNumber,
+        blockHash: event.blockHash,
+        eventTime: event.args.time.toNumber(),
+        transactionIndex: event.transactionIndex,
+        transactionHash: event.transactionHash,
+        logIndex: event.logIndex,
+      } as EventRow;
     });
+  });
+  const eventRows = (await Promise.all(eventsRowsPromises)).flat();
 
-    const logsDecoded = abiDecoder.decodeLogs(logs);
+  eventRows.sort((left, right) => left.blockNumber - right.blockNumber);
 
-    for (let i = 0; i != logsDecoded.length; i++) {
-      const logDecoded = logsDecoded[i];
-      const logRaw = logs[i];
-
-      if (!["TokensMinted", "TokensBurned"].includes(logDecoded.name)) {
-        continue;
-      }
-
-      const event = {
-        eventName: logDecoded.name,
-        amount: logDecoded.events.find((i) => i.name == "amount").value,
-        ethAmount: logDecoded.events.find((i) => i.name == "ethAmount").value,
-        blockNumber: logRaw.blockNumber,
-        blockHash: logRaw.blockHash,
-        blockTime: Number(
-          logDecoded.events.find((i) => i.name == "time").value
-        ),
-        transactionIndex: logRaw.transactionIndex,
-        transactionHash: logRaw.transactionHash,
-        logIndex: logRaw.logIndex,
-      };
-
-      if (!clb(event)) {
-        return;
-      }
-    }
-  } catch (err) {
-    // TODO: Is this too Infura-specific?
-    if (err.message.includes("query returned more than")) {
-      const middle = Math.round((fromBlock + toBlock) / 2);
-      console.log(
-        "Hit API limit, splitting interval to [%s, %s] and [%s, %s]",
-        fromBlock,
-        middle,
-        middle + 1,
-        toBlock
-      );
-
-      await readEventsFromETHRangeWithFallback(fromBlock, middle, clb);
-      await readEventsFromETHRangeWithFallback(middle + 1, toBlock, clb);
-    } else {
-      throw err;
-    }
+  if (eventRows.length > 0) {
+    return onEventsBatchCallback(eventRows);
+  } else {
+    return true;
   }
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
-// See: https://etherscan.io/token/0xae78736cd615f374d3085123a210448e74fc6393
-const rethAddress = "0xae78736cd615f374d3085123a210448e74fc6393";
-const rethStartBlock = 13325304;
+function getProvider(url: string): Provider {
+  const { protocol } = new URL(url);
+  const isWebSocketSchema = /wss?:/.test(protocol);
 
-// ABI
-const rethAbi = JSON.parse(fs.readFileSync("./abi/reth.abi.json", "utf8"));
-abiDecoder.addABI(rethAbi);
-
-// Init
-const providerUrl = process.env.ETH_NODE_PROVIDER_URL;
-console.log("ETH node provider URL: %s", providerUrl);
-const web3 = new Web3(
-  new Web3.providers.WebsocketProvider(providerUrl, {
-    clientConfig: {
-      maxReceivedFrameSize: 10000000,
-      maxReceivedMessageSize: 10000000,
-    },
-  })
-);
-
-// Block range
-const etag = process.env.ODF_ETAG;
-const newEtagPath = process.env.ODF_NEW_ETAG_PATH;
-
-const lastSeenBlock = etag.length != 0 ? Number(etag) : 0;
-console.log("Last seen block: %s", lastSeenBlock);
-
-const ethHeadBlock = await web3.eth.getBlockNumber();
-console.log("Current chain head block: %s", ethHeadBlock);
-
-let stride;
-if (process.env.STRIDE) {
-  stride = Number(process.env.STRIDE);
-} else {
-  stride = undefined;
+  if (isWebSocketSchema) {
+    return new ethers.providers.WebSocketProvider(url);
+  } else {
+    return new ethers.providers.JsonRpcProvider(url);
+  }
 }
 
-await readEventsFromETH(
-  Math.max(lastSeenBlock + 1, rethStartBlock),
-  ethHeadBlock,
-  function (event) {
-    process.stdout.write(JSON.stringify(event));
-    process.stdout.write("\n");
-    return true;
-  },
-  stride
-);
+/////////////////////////////////////////////////////////////////////////////////////////
 
-// Save last seen block
-fs.writeFileSync(newEtagPath, ethHeadBlock.toString());
-process.exit(0);
+async function destroy(provider: Provider): Promise<void> {
+  if (provider instanceof ethers.providers.WebSocketProvider) {
+    await provider.destroy();
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+function sanitizeEnvMiddleware(env: NodeJS.ProcessEnv): NodeJS.ProcessEnv {
+  for (const key in env) {
+    const value = env[key];
+
+    if (value === '') {
+      delete env[key];
+    }
+  }
+
+  return env;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+main().catch(console.log);
